d3f69013a79321a6334778c3d54392e232fdee85
sheltowt <sheltowt@gmail.com>
2016-11-08 14:39:50 -0700

    adding updates

--- merge_sort/merge_best.py
+++ merge_sort/merge_best.py
@@ -1,13 +1,13 @@
-# this is a great merge sort
 
-def msort2(x):
-    if len(x) < 2:
-        return x
+
+def msort(li):
+    if len(li) < 2:
+        return li
     result = []
-    mid = int(len(x)/2)
-    y = msort2(x[:mid])
-    z = msort2(x[mid:])
-    while (len(y) > 0) and (len(z) > 0):
+    mid = int(len(li) / 2)
+    y = msort(li[:mid])
+    z = msort(li[mid:])
+    while (len(y) > 0 and len(z) > 0):
         if y[0] > z[0]:
             result.append(z[0])
             z.pop(0)
@@ -16,28 +16,9 @@ def msort2(x):
             y.pop(0)
     result += y
     result += z
-    print result
-
-
-
-def msort(x):
-    if len(x) < 2:
-        return x
-    result = []          # moved!
-    mid = int(len(x)/2)
-    y = msort(x[:mid])
-    z = msort(x[mid:])
-    while (len(y) > 0) and (len(z) > 0):
-            if y[0] > z[0]:
-                result.append(z[0])
-                z.pop(0)
-            else:
-                result.append(y[0])
-                y.pop(0)
-    result += y
-    result += z
-    print result
     return result
+
+
 
 n_sort = [1, 4, 3, 2, 5, 6, 8, 4, 5, 6]
 print msort(n_sort)
